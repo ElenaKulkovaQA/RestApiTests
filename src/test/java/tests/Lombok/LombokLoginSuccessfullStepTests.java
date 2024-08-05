466ea2c21fbc725cce331a4d511fb61fe9c990d2
Elena <elena.regiionh@gmail.com>
2024-08-05 18:44:29 +0300

    added check token isNotNull

--- src/test/java/tests/Lombok/LombokLoginSuccessfullStepTests.java
+++ src/test/java/tests/Lombok/LombokLoginSuccessfullStepTests.java
@@ -8,6 +8,8 @@ import tests.TestBase;
 
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.RequestPackegeSpec.requestSpec;
 import static specs.ResponsePackegeSpec.responseCode200Spec;
@@ -36,12 +38,14 @@ public class LombokLoginSuccessfullStepTests extends TestBase {
 
                                     .then()
                                     .spec(responseCode200Spec)
+                                    .body("token", notNullValue())
                                     .extract().as(LoginResponseModelLombok.class);// размаппить ответ и наложить его на класс LoginResponseModel
                 });
 
         step("Проверить соответствие токена в ответе", () -> {
-            assertEquals("QpwL5tke4Pnpja7X4", response.getToken());// проверить, что токен совпадает с ответом в классе
-
+           assertEquals("QpwL5tke4Pnpja7X4", response.getToken());// проверить, что токен совпадает с ответом в классе
+            //assertThat(response.getToken()).isEqualTo(LoginResponseModelLombok.class);
         });
     }
+
 }
